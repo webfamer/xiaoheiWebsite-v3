b28dcce17697e3713186f3858137bb99f740aad9
xsz <webfamer@gmail.com>
2021-11-19 18:40:14 +0800

    'fix(main.js):添加百度统计代码”

--- src/main.js
+++ src/main.js
@@ -5,3 +5,24 @@ import router from '@/router/index.js'
 // 计算设置rem的基准值，即html的font-size
 import 'amfe-flexible'
 createApp(App).use(Button).use(Swipe).use(SwipeItem).use(Grid).use(GridItem).use(router).use(VanImage).use(NoticeBar).use(Search).use(Tabbar).use(TabbarItem).use(NavBar).use(Field).use(Form).mount('#app')
+
+
+//百度统计代码
+
+var _hmt = _hmt || [];
+window._hmt = _hmt; // 修改为window 全局变量
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f089ba22bcc9f37d1aa1e2af05954a5a";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+router.beforeEach((to, from, next) => {
+  if (window._hmt) {
+    if (to.path) {
+      window._hmt.push(['_trackPageview', '/#' + to.fullPath])
+    }
+  }
+  next()
+})
